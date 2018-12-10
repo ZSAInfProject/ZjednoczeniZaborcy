5fbb766f11c816dfc22c7f27d3684604b9c28226
Kajetan Pynka <kajetanossos1@gmail.com>
2018-12-10 15:49:13 +0100

    Made enemy movement

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    public float Speed = 2f;
+    private float MapHeight;
+
+
+    // Use this for initialization
+    void Start() {
+        Transform BackgroundTransform = GameObject.FindGameObjectWithTag("Background").GetComponent<Transform>();
+        //Jest Half, bo punkt (0,0) jest na srodku ekranu (ale czy powinien byc?)
+        MapHeight = BackgroundTransform.localScale.y;
+        MapHeight -= transform.localScale.y / 2;
+
+        if (transform.tag == "PlayerBullet") {
+            //rb.velocity = new Vector2(0, 10); 
+        }
+    }
+
+    void Update() {
+        if (Mathf.Abs(transform.position.y) >= MapHeight) {
+            Destroy(gameObject);
+        }
+        transform.Translate(0, 0.1f, 0);
+    }
+
+    private void OnTriggerEnter2D (Collider2D other) {
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    enum Direction{
+        Left = -1,
+        Right = 1
+    }
+
+    public float Speed = 8f;
+    private float MapWidth;
+    public GameObject Bullet;
+    private Direction Dir;
+    // Use this for initialization
+    void Start() {
+        Transform BackgroundTransform = GameObject.FindGameObjectWithTag("Background").GetComponent<Transform>();
+        MapWidth = BackgroundTransform.localScale.x;
+        MapWidth -= (transform.localScale.x / 2);
+        Dir = Direction.Right; 
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector2 Move = new Vector2((int)Dir * Speed * Time.deltaTime, 0);
+        transform.Translate(Move);
+        float ClampedX = Mathf.Clamp(transform.position.x, (0 + transform.localScale.x / 2), MapWidth);
+        transform.position = new Vector2(ClampedX, transform.position.y);
+
+        if (transform.position.x == MapWidth){
+            Dir = Direction.Left;
+        }
+        else if (transform.position.x == 0.5f){
+            Dir = Direction.Right;
+        }
+    }
+}
